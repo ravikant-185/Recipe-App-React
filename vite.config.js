ce32eedee75836459694c49ab16338cbfff3edc1
Ravikant Pandey <ravikant185@gmail.com>
2025-08-21 17:47:21 +0530

    Your commit message describing changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// For GitHub Pages, set the base to your repo name
 export default defineConfig({
-  // base: '/Recipe-App/',
+  base: '/Recipe-App/',
   plugins: [react()],
-})
+});

7f0df9bc2cb798a7947c787d71fa92a1345ea355
Ravikant Pandey <ravikant185@gmail.com>
2025-08-22 12:34:36 +0530

    Fix: added basename for GitHub Pages routing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// For GitHub Pages, set the base to your repo name
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Recipe-App/',
   plugins: [react()],
-});
+  base: '/Recipe-App-React/',
+})
